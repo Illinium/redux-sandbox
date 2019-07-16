13228565e49e8f86829d673870bc6079adfbac20
Illinium <khodorko88@gmail.com>
2019-07-16 09:18:58 +0300

    redux

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+
+  switch (action.type) {
+      case 'INC':
+          return state +1;
+      case 'DEC':
+          return state -1;
+      default:
+          return state;
+  }
+};
+
+const store = createStore(reducer);
+
+document.getElementById('dec')
+    .addEventListener('click', () => {
+       store.dispatch({type: 'DEC'});
+    });
+
+document.getElementById('inc')
+    .addEventListener('click', () => {
+        store.dispatch({type: 'INC'});
+    });
+
+const update = () => {
+    document.getElementById('counter')
+        .innerHTML = store.getState();
+};
+
+store.subscribe(update);
+
+
